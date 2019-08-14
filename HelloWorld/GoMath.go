45233870bef3603f3fbca1c737c6b9ffb2b23052
Vaibhav <vaibhavkulkar@gmail.com>
2019-08-14 08:02:48 +0530

    GoMath

---
+++ HelloWorld/GoMath.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Printf("Now you have %g problems.\n", rand.Intn(30))
+
+	var i, j int = 1, 2
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
+}
